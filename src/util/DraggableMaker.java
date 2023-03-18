528162eec7b96704aaa3993c3156316e54af18f2
ricardofernandes21 <114725680+ricardofernandes21@users.noreply.github.com>
2023-03-19 00:57:51 +0100

    Some tries about anchor pane stuff

--- src/util/DraggableMaker.java
+++ src/util/DraggableMaker.java
@@ -1,6 +1,35 @@
 package util;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+
+import javax.swing.text.html.ImageView;
+
 public class DraggableMaker
 {
 
+  /*public DraggableMaker(ImageView imageView )
+  {
+    imageView.setOnDragDetected(new EventHandler<MouseEvent>() {
+      public void handle(MouseEvent event) {
+        /* drag was detected, start a drag-and-drop gesture*/
+        /* allow any transfer mode */
+       // Dragboard db = source.startDragAndDrop(TransferMode.ANY);
+
+        /* Put a string on a dragboard */
+        /*ClipboardContent content = new ClipboardContent();
+        content.putString(source.getText());
+        db.setContent(content);
+
+        event.consume();
+      }
+    });
+
+  }
+  */
 }
+
+
--- src/view_client/ChessViewController.java
+++ src/view_client/ChessViewController.java
@@ -1,15 +1,27 @@
 package view_client;
 
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 import viewmodel_client.ChessViewModel;
+import javafx.scene.input.*;
+import javafx.event.EventHandler;
+
+import java.lang.ref.WeakReference;
 
 public class ChessViewController  {
 
 	protected Region root;
+	private double orgSceneX, orgSceneY;
+	private double orgTranslateX, orgTranslateY;
 
-	protected ViewHandler viewHandler;
+	private ViewHandler viewHandler;
 
 	protected ChessViewModel viewModel;
+
+	@FXML private ImageView queen;
 
 
 
@@ -17,7 +29,22 @@ public class ChessViewController  {
 		this.viewHandler=viewHandler;
 		this.viewModel=viewModel;
 		this.root=root;
+		this.orgSceneX = 0;
+		this.orgSceneY= 0;
+		this.orgTranslateX =0;
+		this.orgTranslateY=0;
+
+		makeDraggable(queen);
+
+
+
+
+
+
+
 	}
+
+
 
 	public Region getRoot(){
 		return root;
@@ -27,5 +54,90 @@ public class ChessViewController  {
 	public void reset() {
 
 	}
+
+	private void makeDraggable(ImageView imageView)
+	{
+
+
+
+		imageView.setOnMousePressed(new EventHandler<MouseEvent>()
+		{
+			AnchorPane targetPane = null;
+
+			@Override public void handle(MouseEvent event)
+			{
+				// Store the initial coordinates of the mouse click
+				orgSceneX = event.getSceneX();
+				orgSceneY = event.getSceneY();
+				orgTranslateX = imageView.getTranslateX();
+				orgTranslateY = imageView.getTranslateY();
+
+				targetPane.setPrefSize(200, 200);
+				targetPane.setLayoutX(event.getSceneX());
+				targetPane.setLayoutY(event.getSceneY());
+
+			}
+		});
+
+		imageView.setOnMouseDragged(new EventHandler<MouseEvent>()
+		{
+			@Override public void handle(MouseEvent event)
+			{
+				// Calculate the new position of the ImageView
+				double offsetX = event.getSceneX() - orgSceneX;
+				double offsetY = event.getSceneY() - orgSceneY;
+				double newTranslateX = orgTranslateX + offsetX;
+				double newTranslateY = orgTranslateY + offsetY;
+
+				// Set the new position of the ImageView
+				imageView.setTranslateX(newTranslateX);
+				imageView.setTranslateY(newTranslateY);
+			}
+		});
+
+
+		AnchorPane anchorPane = new AnchorPane();
+
+
+		// Set the event handler for mouse release events on the ImageView
+		imageView.setOnMouseReleased(new EventHandler<MouseEvent>()
+		{
+			@Override public void handle(MouseEvent event)
+			{
+				AnchorPane targetPane = new AnchorPane();
+				targetPane.setPrefSize(200, 200);
+				targetPane.setLayoutX(event.getSceneX());
+				targetPane.setLayoutY(event.getSceneY());
+				anchorPane.getChildren().add(targetPane);
+
+
+				// Check if the ImageView is within the bounds of the target AnchorPane
+				if (event.getSceneX() > targetPane.getLayoutX() && event.getSceneX()
+						< targetPane.getLayoutX() + targetPane.getPrefWidth()
+						&& event.getSceneY() > targetPane.getLayoutY() && event.getSceneY()
+						< targetPane.getLayoutY() + targetPane.getPrefHeight())
+				{
+					// Remove the ImageView from the root AnchorPane
+					targetPane.getChildren().remove(imageView);
+
+					// Add the ImageView to the target AnchorPane
+					anchorPane.getChildren().add(imageView);
+
+					// Set the position of the ImageView relative to the target AnchorPane
+					imageView.setLayoutX(event.getSceneX() - targetPane.getLayoutX());
+					imageView.setLayoutY(event.getSceneY() - targetPane.getLayoutY());
+				}
+				else
+				{
+					// If the ImageView is not within the bounds of the target AnchorPane, return it to its original position
+					imageView.setTranslateX(orgTranslateX);
+					imageView.setTranslateY(orgTranslateY);
+				}
+			}
+
+		});
+	}
+
+
 
 }
